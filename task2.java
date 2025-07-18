58d04da9fd5be18b2b8ddb3e1e6d5aba53c8ab82
vyshu1702 <134031310+vyshu1702@users.noreply.github.com>
2025-07-18 16:14:57 +0530

    Create task2.java

---
+++ task2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            if (marks[i] < 0 || marks[i] > 100) {
+                System.out.println("Invalid mark. Please enter a value between 0 and 100.");
+                i--; 
+                continue;
+            }
+            totalMarks += marks[i];
+        }
+        double averagePercentage = (double) totalMarks / numSubjects;
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else if (averagePercentage >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+        System.out.println("\n--- Result ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+        scanner.close();
+    }
+}
